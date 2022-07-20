a7f2494efdf839e484fc09601c08b7bbb819928a
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-07-20 19:20:38 -0400

    Get user data added + change on the dialog button

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,14 +1,62 @@
-import { StyleSheet, SafeAreaView, View, Text, Pressable } from "react-native";
+import { StyleSheet, ScrollView, View, Text, Pressable, TextInput, Image, RefreshControl } from "react-native";
+import { useState, useEffect, useCallback } from "react"
 import { useAuthContext } from "../contexts/auth";
 import * as SecureStore from "expo-secure-store";
+import { getUserDataAPI } from "../api/user"
+import { ALERT_TYPE, Dialog } from 'react-native-alert-notification';
 
 export default function ProfileScreen() {
   const { setIsAuth } = useAuthContext();
+  const [userData, setUserData] = useState(null)
+  const [refreshing, setRefreshing] = useState(false);
+
+  const notify = (type, title, message) => {
+    return (Dialog.show({ type, title, textBody: message, button: 'Ok', }))
+  }
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await getUserData()
+    setRefreshing(false)
+  }, []);
+
+  useEffect(() => {
+    setRefreshing(true);
+    getUserData()
+    setRefreshing(false);
+  }, [])
+
+  const getUserData = async () => {
+    const response = await getUserDataAPI()
+
+    if (response?.response?.status >= 401) {
+      await SecureStore.deleteItemAsync("token")
+      return setIsAuth(false)
+    }
+
+    if (!response.success) {
+      return notify(ALERT_TYPE.WARNING, "Ups, ocurrió un error", "Por favor, intenta más tarde.")
+    }
+
+    if (response.success) {
+      const { driverName, driverLastname, driverIdentificationCode, driverPhoto, driverDateOfBirth, driverEmail } = response.data
+      setUserData({
+        name: driverName,
+        lastname: driverLastname,
+        identificationCode: driverIdentificationCode,
+        photo: driverPhoto,
+        dateOfBirth: driverDateOfBirth,
+        email: driverEmail
+      })
+    }
+  }
 
   const onPress = async () => {
     await SecureStore.deleteItemAsync("token");
     setIsAuth(false);
   };
+
+  console.log(userData)
 
   return (
     <View style={styles.container}>
@@ -16,19 +64,45 @@ export default function ProfileScreen() {
         <Text style={styles.headerText}>Perfil</Text>
       </View>
 
-      <View style={styles.profileContainer}>
-        <Pressable style={styles.button} onPress={onPress}>
-          <Text style={styles.buttonText}>Cerrar sesión</Text>
-        </Pressable>
-      </View>
+      {userData && (
+        <ScrollView style={styles.profileContainer} contentContainerStyle={{ padding: 16 }} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+          <View style={styles.photoContainer}>
+            <Image style={styles.photo} source={{ uri: userData.photo }} />
+          </View>
+
+          <View style={styles.formGroup}>
+            <Text style={styles.formLabel}>Nombre completo:</Text>
+            <TextInput style={styles.formInput} editable={false} defaultValue={`${userData.name} ${userData.lastname}`} />
+          </View>
+
+          <View style={styles.formGroup}>
+            <Text style={styles.formLabel}>Cédula:</Text>
+            <TextInput style={styles.formInput} editable={false} defaultValue={userData.identificationCode} />
+          </View>
+
+          <View style={styles.formGroup}>
+            <Text style={styles.formLabel}>Fecha de nacimiento:</Text>
+            <TextInput style={styles.formInput} editable={false} defaultValue={String(userData.dateOfBirth)} />
+          </View>
+
+          <View style={styles.formGroup}>
+            <Text style={styles.formLabel}>Correo:</Text>
+            <TextInput style={styles.formInput} editable={false} defaultValue={userData.email} />
+          </View>
+
+          <Pressable style={styles.button} onPress={onPress}>
+            <Text style={styles.buttonText}>Cerrar sesión</Text>
+          </Pressable>
+        </ScrollView>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
+    flex: 1
   },
   header: {
     height: 100,
@@ -42,7 +116,32 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
   profileContainer: {
-    padding: 16
+    flex: 1
+  },
+  photoContainer: {
+    alignItems: "center",
+    marginBottom: 16
+  },
+  photo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50 
+  },  
+  formGroup: {
+    marginBottom: 16,
+  },
+  formLabel: {
+    marginBottom: 8,
+    fontSize: 16,
+    color: "#6B7280",
+  },
+  formInput: {
+    backgroundColor: "#F2F2F2",
+    // borderWidth: 2, 
+    // borderColor: "#E5E7EB", 
+    height: 48, 
+    paddingHorizontal: 16, 
+    borderRadius: 4,
   },
   button: {
     alignItems: "center",
